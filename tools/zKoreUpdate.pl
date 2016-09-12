8e3b638daa02d719cc1da0924ad088c886acd3cf
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2016-09-12 20:53:10 -0300

    Added del_tree sub&routine

--- tools/zKoreUpdate.pl
+++ tools/zKoreUpdate.pl
@@ -2,18 +2,30 @@
 use strict;
 use warnings;
 use File::Copy;
+use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 use File::Path qw(remove_tree);
 use Switch;
 
 #How it works : http://tinypic.com/view.php?pic=2cwwg14&s=8
 #It will copy the selected file for all folders in our directory. 
+my ($exception, $counter) = 0;
 *principal = *main;
 principal();
-system("pause");
 
 sub main {
+my $delete = 0;
 	system("cls");
-	printf("Select a dir/file for update:\n\n1 => src/tables\n2 => control/macros.txt\n3 => control/config.txt\n4 => control/pickupitems.txt\n5 => control/items_control.txt\n6 => control/mon_control.txt\n7 => tables/bRO/portals.txt\n8 => A plugin\n9 => Another file\n\nR: ");chomp (my $response = <STDIN>);switch ($response) { case 1 {main1(); } case 2 {main2("macros");} case 3 {main2("config");} case 4 {main2("pickup");} case 5 {main2("itemsc");}  case 6 {main2("monc");} case 7 {main2("portals");} case 8 {main2("plugins");} case 9 {main2();} case "exit" { exit (0); } else  { system"cls";print "Try again\n";sleep(1);main(); } }
+	printf("Select a dir/file for update:\n\n1 => src/tables\n2 => control/macros.txt\n3 => control/config.txt\n4 => control/pickupitems.txt\n5 => control/items_control.txt\n6 => control/mon_control.txt\n7 => tables/bRO/portals.txt\n8 => A plugin\n9 => Another file\n10 (fld) => Delete folder\n\nR: ");chomp (my $response = <STDIN>);
+	switch ($response) { 
+		case 1 {main1(); } case 2 {main2("macros");} case 3 {main2("config");} 
+		case 4 {main2("pickup");} case 5 {main2("itemsc");}  case 6 {main2("monc");} 
+		case 7 {main2("portals");} case 8 {main2("plugins");} case 9 {main2();} 
+		case "exit" { exit (0); 
+	} 
+	if ($response =~ /10 (.*)/ig) {$delete = $1;deltree("delete $delete");}
+	elsif ($response =~ /^(\d+)?(\s+)?$/ig)  { system"cls";print "Try again\n";my $num = $1;print "Missing folder argument to delete and replace by" if ($num == "10");sleep(1);main(); }
+	}
+	system("pause");
 }
 
 sub main1 {
@@ -33,19 +45,19 @@ sub main1 {
 
 sub main1a {
 	system("cls");
-	my @files = <*>;
-	my $counter = 0;
-	foreach my $files (@files) {
+	foreach (<*>) {
 	my ( $receive_from, $receive_to,  $send_from,  $send_to,  $packets_from,  $packets_to , $x, $y, $z);
 		if (-d "src" && -d "tables") {
-			next if ($files =~ /^.+\.pl$/ig);
+			next if ($_ =~ /^.+\.pl$/ig);
 			$receive_from = "src/Network/Receive/bro.pm";	
-			$receive_to = "$files/$receive_from";
-			$send_from = "src/Network/Send/bro.pm";
-			$send_to = "$files/$send_from";
-			$packets_from = "tables/bro/recvpackets.txt";
-			$packets_to = "$files/$packets_from";
+			$receive_to = "$_/$receive_from";
 			#
+			$send_from = "src/Network/Send/bro.pm";
+			$send_to = "$_/$send_from";
+			#
+			$packets_from = "tables/bro/recvpackets.txt";
+			$packets_to = "$_/$packets_from";
+			#translate
 			$receive_to =~ s/bro/bRO/i;
 			$send_to =~ s/bro/bRO/i;
 			$packets_to =~ s/bro/bRO/i;
@@ -54,7 +66,7 @@ sub main1a {
 			$y = copy("$send_from", "$send_to");
 			$z = copy("$packets_from", "$packets_to");
 				if ($x == $y && $x == $z) {
-				#print "$files = $x\n"  unless ($files =~ /src|tables/ig);
+				#print "$_ = $x\n"  unless ($_ =~ /src|tables/ig);
 				$counter += 1;
 				}
 		} else {
@@ -66,14 +78,14 @@ return $counter;
 
 sub main2 {
 	system("cls");
-	my ($file, $destin, $sys_from, $sys_to) = 0;
-	my @files = <*>;
+	my ($file, $destin, $sys_from, $sys_to, $pname) = 0;
 	print "Plugin name (with/out .pl extension) : " if (defined($_[0]) && $_[0] eq "plugins");
+	chomp ($pname=<STDIN>) if (defined($_[0]) && $_[0] eq "plugins");
 	$file = "plugins" if (defined($_[0]) && $_[0] eq "plugins");
 	$file = shift if (defined($_[0]) && $_[0] ne "plugins");
 	print "File : " if (!$file);
 	chomp ($file=<STDIN>) if (!$file);
-
+	#$pname .= ".pl" if ($pname !~ /(.*)\.pl/ig);
 	if ($file =~ /macros.txt|macros/ig) {
 		$sys_from = "macros.txt";
 		goto controlfiles;
@@ -93,7 +105,7 @@ sub main2 {
 		$sys_from = "portals.txt";
 		goto tablesfiles;
 	}	elsif ($file =~ /plugins|plugns/ig) {
-		$sys_from = $file;
+		$sys_from = $pname;
 		goto plugnsfolder;
 	}
 		else {	
@@ -107,38 +119,71 @@ sub main2 {
 				else {
 				$sys_from = $file;
 				$sys_to = "$destin/$sys_from";
-				movefiles($sys_from, $sys_to , @files);
+				movefiles($sys_from, $sys_to);
 				return 1;
 				}
 		}
 	controlfiles:
-	movefiles($sys_from, "control/$sys_from", @files);
+	movefiles($sys_from, "control/$sys_from");
 
 	tablesfiles:
-	movefiles($sys_from, "tables/bRO", @files);
+	movefiles($sys_from, "tables/bRO");
 	
 	plugnsfolder:
-	movefiles($sys_from, "/plugins", @files);
+	movefiles($sys_from, "plugins");
 }
 
 sub movefiles {
-my ( $sys_from, $sys_to, @files) = @_;
-my ($exception, $counter) = 0;
-		foreach my $files (@files) {
-			if (-f "$sys_from") {
-				if (-d $files) {
-					$counter += copy ("$sys_from", "$files/$sys_to");
-					#print "$sys_from => $files/$sys_to\n";
-					}
-				else {
-				$exception += 1;
+my ( $sys_from, $sys_to) = @_;
+	foreach (<*>) {
+	next if ($_ =~ /zPlugins/ig);
+		if (-f "$sys_from") {
+			if (-d "$_") {
+				$counter += copy ("$sys_from", "$_/$sys_to");
+				#print "$sys_from => $_/$sys_to\n";
 				}
+			else {
+			$exception += 1;
 			}
 		}
-			system("cls");
-			if ($counter) {
-			print "Sucess in \@bRO all folders\n\n";
-			} else {
-			print "Failed in update. Check if the \'$sys_from\' file exists.\n\n";
+	}
+		system("cls");
+		if ($counter > 0) {
+		print "Sucess in \@bRO all folders\n\n";
+		} else {
+		print "Failed in update. Check if the \'$sys_from\' file exists.\n\n";
+		}
+		unlink $sys_from;
+	return 1;
+}
+
+sub deltree {
+my $sys_from = shift;
+$sys_from =~ s/delete (.*)/$1/ig;
+	foreach (<*>) {
+	next if ($_ =~ /(z)?Plugins|Control Panel/ig); #Hidden file or Plugin folder or any extra folder that isn't a botter folder!
+		if (-d "$sys_from") {
+			if (-d "$_") {
+				if (-d "$_/plugins") {
+				$counter += remove_tree($_.'/'.$sys_from);
+				} else {
+				$counter += dircopy("plugins", "$_/plugins");
+				}
+				#print "$sys_from => $_/$sys_to\n";
+				}
+			else {
+			$exception += 1;
 			}
+		} else {
+			print "$sys_from not found\n";
+			return 0;
+		}
+	}
+	if ($counter > 0) {
+		print "Sucess in \@bRO all folders\n\n";
+		} else {
+		print "Failed in update. Check if the \'$sys_from\' file exists.\n\n";
+	}
+	remove_tree($sys_from);
+	return 1;
 }
