f87f94ab4a92120458a49e8b3542e7d38ed9eb88
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2016-11-23 01:15:11 -0200

    Update st_timeout.pm

--- tips/st_timeout.pm
+++ tips/st_timeout.pm
@@ -6,10 +6,17 @@
 package st_timeout;
 use strict;
 use warnings;
+use Log qw (message);
 
 Plugins::register("[OPK-TUTO] Timeout example", "Periodically checks", \&on_unload, \&on_reload);
+
+my $timeout = 10;
 my $phook = Plugins::addHook("AI_pre", \&onLoop);
-my %checkTimeout = ( time => time(), timeout => 10 );
+my %checkTimeout = ( time => time(), timeout => $timeout );
+
+sub on_unload { Plugins::delHook($phook); }
+
+sub on_reload { on_unload(); }
 
 sub onLoop {
 	if ( timeOut (\%checkTimeout) ) { #If last time called was 10s ago, we can next step
@@ -20,3 +27,5 @@ sub onLoop {
 sub prepare {
   #do your editions
 }
+
+1;
