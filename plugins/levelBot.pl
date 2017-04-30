c4c0f819e6e4de724ea1ae86583beb935c686eb9
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2017-04-30 15:10:40 -0300

    Update levelBot.pl

--- plugins/levelBot.pl
+++ plugins/levelBot.pl
@@ -20,7 +20,7 @@ my $phook = Plugins::addHooks(
 my $logHook = Log::addHook(\&on_Log, "levelBot");
 my $datadir = $Plugins::current_plugin_folder;
 my $commands_handle = Commands::register(
-	['ms', 'Prepare merchant to sell', \&on_ms]
+['ms', 'Prepare merchant to sell', \&on_ms]
 );
 
 my %checkTimeout = ( time => time(), timeout => 5 );
@@ -41,18 +41,18 @@ warning "[levelBot-plugin] - Reloaded\n";
 
 sub on_Log {
 my ($type, $domain, $level, $globalVerbosity, $message, $user_data) = @_;
-	if( $type eq "message" ) {
-		if ($message =~ /ponto de retorno/ig) {	#Return point message from kafra in console!
-			Commands::run("conf saveMap ". $field->baseName());
-			$saved = 1;
-		}
-	}
+if( $type eq "message" ) {
+if ($message =~ /ponto de retorno/ig) {	#Return point message from kafra in console!
+Commands::run("conf saveMap ". $field->baseName());
+$saved = 1;
+}
+}
 }
 
 sub onLoop {
-	if ( timeOut (\%checkTimeout) ) {
-	prepare();
-	}
+if ( timeOut (\%checkTimeout) ) {
+prepare();
+}
 
 }
 
@@ -63,9 +63,9 @@ return 1;
 
 sub run_commands {
 my $x = shift;
-	for (@commands) {
-		Commands::run("$x$_");
-	}
+for (@commands) {
+Commands::run("$x$_");
+}
 undef @commands;
 }
 
@@ -88,95 +88,95 @@ _command_add("useSelf_item_0 Poção Vermelha, Poção Laranja, Poção de Apren
 _command_add("useSelf_item_0_hp < 70%");
 my $char_lv = $char->{'lv'};
 my $char_jb = $char->{'lv_job'};return 2 if (_check_class() eq "Merchant");
-	goto savemap;
-	savemap:
-	$checkTimeout{time} = time();
-	if (( $config{'saveMap'} ne "geffen" || !$config{'saveMap'}) || $config{'lockMap'} eq "gef_fild06") {
-		if ($char_lv < 80 && $char_lv >= 65) {
-						_command_add("lockMap gef_fild06");
-						_command_add("storageAuto_npc geffen 203 123");
-						_command_add("buyAuto_0_npc geffen_in 77 167");
-						_command_add("sellAuto_npc geffen_in 77 167");
+goto savemap;
+savemap:
+$checkTimeout{time} = time();
+if (( $config{'saveMap'} ne "geffen" || !$config{'saveMap'}) || $config{'lockMap'} eq "gef_fild06") {
+if ($char_lv < 80 && $char_lv >= 65) {
+_command_add("lockMap gef_fild06");
+_command_add("storageAuto_npc geffen 203 123");
+_command_add("buyAuto_0_npc geffen_in 77 167");
+_command_add("sellAuto_npc geffen_in 77 167");
 run();
 _save_map("geffen 203 123");
-		}
-	}
-	if (($config{'saveMap'} ne "payon" || !$config{'saveMap'}) || $config{'lockMap'} =~ /pay.*/ig) {
-		if ($char_lv < 19) {
+}
+}
+if (($config{'saveMap'} ne "payon" || !$config{'saveMap'}) || $config{'lockMap'} =~ /pay.*/ig) {
+if ($char_lv < 19) {
 
-				if (_check_class() == "High Novice") {
-					_command_add("sellAuto 0");
-					_command_add("storageAuto 0");
-				} else {
-					_command_add("sellAuto 1");
-					_command_add("storageAuto 1");
-				}
-				_command_add("sellAuto_npc payon_in01 5 49");
-				_command_add("storageAuto_npc payon 181 104");
-					if (_zeny() >= 7500) {
-						_command_add("buyAuto_0_disabled 0");
-					} else {
-						_command_add("buyAuto_0_disabled 1");
-					}
-						_command_add("buyAuto_0_npc payon_in01 5 49");
-						_command_add("lockMap pay_fild08");
-		} elsif ($char_lv < 25 && $char_lv >= 19) {
-						_command_add("sellAuto_npc payon_in01 5 49");
-						_command_add("buyAuto_0_npc payon_in01 5 49");
-						_command_add("storageAuto_npc payon 181 104");
-						_command_add("lockMap pay_fild07");
-		} elsif ($char_lv < 43 && $char_lv >= 25) {
-						_command_add("lockMap pay_fild09");
-		}
+if (_check_class() == "High Novice") {
+_command_add("sellAuto 0");
+_command_add("storageAuto 0");
+} else {
+_command_add("sellAuto 1");
+_command_add("storageAuto 1");
+}
+_command_add("sellAuto_npc payon_in01 5 49");
+_command_add("storageAuto_npc payon 181 104");
+if (_zeny() >= 7500) {
+_command_add("buyAuto_0_disabled 0");
+} else {
+_command_add("buyAuto_0_disabled 1");
+}
+_command_add("buyAuto_0_npc payon_in01 5 49");
+_command_add("lockMap pay_fild08");
+} elsif ($char_lv < 25 && $char_lv >= 19) {
+_command_add("sellAuto_npc payon_in01 5 49");
+_command_add("buyAuto_0_npc payon_in01 5 49");
+_command_add("storageAuto_npc payon 181 104");
+_command_add("lockMap pay_fild07");
+} elsif ($char_lv < 43 && $char_lv >= 25) {
+_command_add("lockMap pay_fild09");
+}
 run();
 _save_map("payon 181 104");
-	}
-	if (($config{'saveMap'} ne "prontera" || !$config{'saveMap'}) || $config{'lockMap'} eq "gef_fild10") {
-		if ($char_lv < 50 && $char_lv >= 43) {
-			_command_add("sellAuto_npc prt_in 126 76");
-			_command_add("buyAuto_0_npc prt_in 126 76");
-			_command_add("storageAuto_npc prontera 151 29");
-			_command_add("lockMap gef_fild10");
+}
+if (($config{'saveMap'} ne "prontera" || !$config{'saveMap'}) || $config{'lockMap'} eq "gef_fild10") {
+if ($char_lv < 50 && $char_lv >= 43) {
+_command_add("sellAuto_npc prt_in 126 76");
+_command_add("buyAuto_0_npc prt_in 126 76");
+_command_add("storageAuto_npc prontera 151 29");
+_command_add("lockMap gef_fild10");
 run();
 _save_map("prontera 151 29");
-		}
-	} 
-	if (($config{'saveMap'} ne "yuno" || !$config{'saveMap'}) || $config{'lockMap'} eq "yuno_fild03") {
-		if ($char_lv >= 80 && $char_lv < 99) {
-					_command_add("sellAuto_npc yuno 218 97");
-					_command_add("buyAuto_0_npc yuno 218 97");
-					_command_add("storageAuto_npc yuno 152 187");
-					_command_add("lockMap yuno_fild03");
+}
+}
+if (($config{'saveMap'} ne "yuno" || !$config{'saveMap'}) || $config{'lockMap'} eq "yuno_fild03") {
+if ($char_lv >= 80 && $char_lv < 99) {
+_command_add("sellAuto_npc yuno 218 97");
+_command_add("buyAuto_0_npc yuno 218 97");
+_command_add("storageAuto_npc yuno 152 187");
+_command_add("lockMap yuno_fild03");
 run();
 _save_map("yuno 152 187");
-		}	
-	} if (($config{'saveMap'} ne "rachel" || !$config{'saveMap'}) || $config{'lockMap'} eq "ve_fild03") {
-		if ($char_lv >= 80 && $char_lv < 99) {
-					_command_add("sellAuto_npc rachel 65 80");
-					_command_add("buyAuto_0_npc ra_in01 257 269");
-					_command_add("storageAuto_npc rachel 109 138");
-					_command_add("lockMap ve_fild03");
+}
+} if (($config{'saveMap'} ne "rachel" || !$config{'saveMap'}) || $config{'lockMap'} eq "ve_fild03") {
+if ($char_lv >= 80 && $char_lv < 99) {
+_command_add("sellAuto_npc rachel 65 80");
+_command_add("buyAuto_0_npc ra_in01 257 269");
+_command_add("storageAuto_npc rachel 109 138");
+_command_add("lockMap ve_fild03");
 run();
 _save_map("rachel 109 138");
-		}
-	} if ($config{'saveMap'}) {
-		on_unload();
-	} else {
-		warning "[levelBot-plugin] - Don't know what to about your up.\n";
-	}
+}
+} if ($config{'saveMap'}) {
+on_unload();
+} else {
+warning "[levelBot-plugin] - Don't know what to about your up.\n";
+}
 end:
 return 1;
 }
 
 sub _timercheck {
 my $weight = $char->{'weight'}/$char->{'weight_max'} * 100;
-	if ($weight > 89 && $config{'attackAuto'} ne 0) {
-	_command_add("attackAuto 0");
-	_command_add("autostorage");
-	_command_add("autosell");
-	} elsif ($weight < 89  && $config{'attackAuto'} ne 2) {
-	_command_add("attackAuto 2");
-	}
+if ($weight > 89 && $config{'attackAuto'} ne 0) {
+_command_add("attackAuto 0");
+_command_add("autostorage");
+_command_add("autosell");
+} elsif ($weight < 89  && $config{'attackAuto'} ne 2) {
+_command_add("attackAuto 2");
+}
 }
 
 sub run {
@@ -195,42 +195,42 @@ if ($map =~ /(\w+) (\d+) (\d+)/ig) {$fieldName = $1;$coords_x = $2;$coords_y = $
 my $want_x = $coords_x - 2;
 my $want_y = $coords_y - 3;
 $config{'attackAuto'} = 0;
-		if ( $field->baseName() ne "$fieldName") {
-		_command_add("move $fieldName $coords_x $coords_y");
-		run_commands();
-		$checkTimeout{time} = time();
-		goto end;
-		} else {
-		$talk = npc_exist("Kafra", $coords_x, $coords_y);
-			if ($talk && $field->baseName() eq "$fieldName") {
-					_command_add("talknpc $talk c r0 n");
-					goto end;
-			} elsif ($field->baseName() eq "$fieldName") {
-					$want_x = eval($coords_x - int(rand(4)));
-					$want_y = eval($coords_y - int(rand(4)));
-					_command_add("move $want_x $want_y");
-					goto end;
-			}
-		}
-		end:
-		run_commands() if (scalar @commands >= 1);
+if ( $field->baseName() ne "$fieldName") {
+_command_add("move $fieldName $coords_x $coords_y");
+run_commands();
+$checkTimeout{time} = time();
+goto end;
+} else {
+$talk = npc_exist("Kafra", $coords_x, $coords_y);
+if ($talk && $field->baseName() eq "$fieldName") {
+_command_add("talknpc $talk c r0 n");
+goto end;
+} elsif ($field->baseName() eq "$fieldName") {
+$want_x = eval($coords_x - int(rand(4)));
+$want_y = eval($coords_y - int(rand(4)));
+_command_add("move $want_x $want_y");
+goto end;
+}
+}
+end:
+run_commands() if (scalar @commands >= 1);
 }
 
 sub npc_exist {
 my ($name, $x, $y) = @_;
 my $msg;
 my $npcs = $npcsList->getItems();
-	foreach my $npc (@{$npcs}) {
-		my $pos = "$npc->{pos}{x} $npc->{pos}{y}";
-				if ($npc->name =~ /.*$name/ig) {
-					if ($pos eq "$x $y") {
-						return $pos;
-					} else {
-						return 0;	
-					}
-				}
-	}
-	return 0;
+foreach my $npc (@{$npcs}) {
+my $pos = "$npc->{pos}{x} $npc->{pos}{y}";
+if ($npc->name =~ /.*$name/ig) {
+if ($pos eq "$x $y") {
+return $pos;
+} else {
+return 0;
+}
+}
+}
+return 0;
 }
 
 sub _check_class {
